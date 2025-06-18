8cd80430b6716cee292a24223a1b58d9f4a6f5cb
angelsamuel21 <codewithangel@gmail.com>
2025-06-18 10:05:47 +0530

    Changed Navbar name correcty

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,7 +14,7 @@ const Navbar = (props) => {
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">
-          iNotebookPro2
+          iNotebookPro
         </Link>
         <button
           className="navbar-toggler"
