c27a3828d13a9ebe79f4acb94cd373f1848ff2ae
angelsamuel21 <codewithangel@gmail.com>
2025-06-21 21:08:45 +0530

    Fixed login problem of WebApp

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -149,7 +149,7 @@ const UserProfile = (props) => {
   };
 
   return (
-    <div className="container mt-5">
+    <div className="container mt-5 mb-3">
       <div className="row justify-content-center">
         <div className="col-md-8 col-lg-7">
           <h1 className="mb-4 text-center">User Profile</h1>
